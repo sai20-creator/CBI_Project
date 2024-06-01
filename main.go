e88d96f7494a9b7232a46d94b61376627b203ec6
Sai Akhil Vudattu <81074678+sai20-creator@users.noreply.github.com>
2024-05-31 21:03:58 -0500

    Update main.go

--- main.go
+++ main.go
@@ -332,7 +332,7 @@ func GetTaxiTrips(db *sql.DB) {
 	// Get your geocoder.ApiKey from here :
 	// https://developers.google.com/maps/documentation/geocoding/get-api-key?authuser=2
 
-	geocoder.ApiKey = "ADD_YOUR_API_KEY_HERE"
+	geocoder.ApiKey = "AIzaSyBSzYYMCGD8Z6xLImd7xHw3kMZ1P_IYeVI"
 
 	drop_table := `drop table if exists taxi_trips`
 	_, err := db.Exec(drop_table)
